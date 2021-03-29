e73663e9fd52131b2cc001e29808848627740b04
Sergey Leschina <mail@putnik.tech>
2021-03-29 18:25:23 +0300

    Improvements for administrative entity qualifiers script

--- adm_entity_clear.py
+++ adm_entity_clear.py
@@ -4,13 +4,32 @@ import pywikibot
 import re
 from pywikibot import pagegenerators as pg
 
-site = pywikibot.Site('ru', 'wikipedia')
-repo = pywikibot.Site('wikidata', 'wikidata')
-
 PID_BIRTH_PLACE = 'P19'
 PID_DEATH_PLACE = 'P20'
 PID_ADM_UNIT = 'P131'
 PID_COUNTRY = 'P17'
+PID_FILE = 'adm_entity_%s'
+REMOVE_SUMMARY = u'Remove administrative entity qualifiers that can be obtained from the target item'
+
+site = pywikibot.Site('ru', 'wikipedia')
+repo = pywikibot.Site('wikidata', 'wikidata')
+processed = {}
+
+
+def mark_processed(qid, pid):
+    processed[pid].append(qid)
+    with open(PID_FILE % pid, 'a') as file:
+        file.write('%s\n' % qid)
+
+
+def is_processed(qid, pid):
+    if pid not in processed:
+        try:
+            with open(PID_FILE % pid) as file:
+                processed[pid] = [row.strip() for row in file]
+        except FileNotFoundError:
+            processed[pid] = []
+    return qid in processed[pid]
 
 
 def load_preview(code):
@@ -26,28 +45,29 @@ def remove_qualifiers(claim):
     if PID_COUNTRY in claim.qualifiers:
         for qualifier in claim.qualifiers[PID_COUNTRY]:
             qualifiers.append(qualifier)
-    claim.removeQualifiers(qualifiers, summary=u'Remove administrative entity qualifiers that can be obtained from the target item')
+    claim.removeQualifiers(qualifiers, summary=REMOVE_SUMMARY)
     return
 
 
-def check_adm_unit(item, place_pid):
+def check_adm_entity(item, place_pid):
+    if is_processed(item.getID(), place_pid):
+        return
     data = item.get()
     if 'claims' not in data or place_pid not in data['claims'] or len(data['claims'][place_pid]) != 1:
         return
     for claim in data['claims'][place_pid]:
         current_html = load_preview('{{wikidata|%s|from=%s}}' % (place_pid, item.getID()))
         new_html = load_preview('{{wikidata/песочница|%s|from=%s}}' % (place_pid, item.getID()))
-#         print(item.getID())
-#         print(current_html)
-#         print(new_html)
         if current_html == new_html:
             print('%s: REMOVE' % item.getID())
             remove_qualifiers(claim)
         else:
             print('%s: SKIP' % item.getID())
+        mark_processed(item.getID(), place_pid)
 
 
 def iterate_items(pid):
+    print(pid)
     query = '''
         SELECT DISTINCT ?item
         WHERE {
@@ -60,7 +80,10 @@ def iterate_items(pid):
     ''' % pid
     generator = pg.WikidataSPARQLPageGenerator(query, site=repo)
     for item in generator:
-        check_adm_unit(item, pid)
+        try:
+            check_adm_entity(item, pid)
+        except:
+            print('%s: OOPS!!!' % item.getID())
 
 
 iterate_items(PID_BIRTH_PLACE)
